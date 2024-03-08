c3a31b1613e7b4f00bd4c0f2ce4977e7c5cc2998
Fekete Boglárka <fekete.boglarka@ckik.loc>
2024-03-08 12:02:49 +0100

    sql

--- adatbazis.sql
+++ adatbazis.sql
@@ -1,3 +1,24 @@
 --Zoli
 
 --Bogi
+
+--Gyár
+CREATE TABLE Gyar(
+    Név varchar(50) PRIMARY KEY,
+    ÁHM int,
+    ÉpítésiÉV int,
+    Település varchar(25),
+    Bevétel int
+);
+
+-- Szett
+CREATE TABLE Szett(
+    Szériaszám int PRIMARY KEY,
+    KiadásiÉv int,
+    Név varchar(50),
+    Ár int,
+    Forgalomban bool,
+    DB int,
+    Téma varchar(50),
+    Kor int
+);
