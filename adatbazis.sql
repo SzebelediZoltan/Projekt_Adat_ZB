311012c9e3c785988030a82d908a4529812b437c
szebeledi.zoltan <szebeledi.zoltan@ckik.hu>
2024-03-08 12:14:00 +0100

    Kész

--- adatbazis.sql
+++ adatbazis.sql
@@ -1,36 +1,16 @@
-CREATE DATABASE Lego_Gyar
-USE Lego_Gyar
+CREATE DATABASE Lego_Gyar;
+USE Lego_Gyar;
 
---Zoli
-CREATE TABLE Alalmazott (
-    TAJ-szám INT PRIMARY KEY
+CREATE TABLE Alkalmazott (
+    TAJ_szám INT PRIMARY KEY,
     Név VARCHAR(30),
     Email VARCHAR(70),
     Fizetés INT,
     SzületésiDátum DATE,
     Telefonszám INT
-)
+);
 
-CREATE TABLE Beosztás (
-    TAJ int NOT NULL,
-    Gyár_név VARCHAR(50) NOT NULL,
-    CONSTRAINT FK_Beosztás_alkalmazott FOREIGN KEY (TAJ) REFERENCES Alkalmazott (TAJ-szám),
-    CONSTRAINT FK_Beosztás_gyár FOREIGN KEY (Gyár_név) REFERENCES Gyár (Név)
-)
-
-CREATE TABLE Gyártási_Sor (
-    Gyár_név VARCHAR(50) NOT NULL,
-    Szériaszám INT NOT NULL,
-    GyártásiÁr INT,
-    ÁHE INT,
-    CONSTRAINT FK_Beosztás_gyár FOREIGN KEY (Gyár_név) REFERENCES Gyár (Név),
-    CONSTRAINT FK_Beosztás_szett FOREIGN KEY (Szériaszám) REFERENCES Szett (Szériaszám)
-)
-
---Bogi
-
---Gyár
-CREATE TABLE Gyar(
+CREATE TABLE Gyár(
     Név varchar(50) PRIMARY KEY,
     ÁHM int,
     ÉpítésiÉV int,
@@ -38,14 +18,30 @@ CREATE TABLE Gyar(
     Bevétel int
 );
 
--- Szett
 CREATE TABLE Szett(
     Szériaszám int PRIMARY KEY,
     KiadásiÉv int,
     Név varchar(50),
     Ár int,
-    Forgalomban bool,
+    Forgalomban boolean,
     DB int,
     Téma varchar(50),
     Kor int
 );
+
+CREATE TABLE Beosztás (
+    TAJ int NOT NULL,
+    Gyár_név VARCHAR(50) NOT NULL,
+    CONSTRAINT FK_Beosztás_alkalmazott FOREIGN KEY (TAJ) REFERENCES Alkalmazott (TAJ_szám),
+    CONSTRAINT FK_Beosztás_gyár FOREIGN KEY (Gyár_név) REFERENCES Gyár (Név)
+);
+
+CREATE TABLE Gyártási_Sor (
+    Gyár_név VARCHAR(50) NOT NULL,
+    Szériaszám INT NOT NULL,
+    GyártásiÁr INT,
+    ÁHE INT,
+    CONSTRAINT FK_Beosztás_gyár2 FOREIGN KEY (Gyár_név) REFERENCES Gyár (Név),
+    CONSTRAINT FK_Beosztás_szett FOREIGN KEY (Szériaszám) REFERENCES Szett (Szériaszám)
+);
+
