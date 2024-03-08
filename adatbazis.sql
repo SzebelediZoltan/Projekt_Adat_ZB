a031c8a7265d058001f34a4db71a791973af8a2a
szebeledi.zoltan <szebeledi.zoltan@ckik.hu>
2024-03-08 12:02:54 +0100

    SQL (Zoli)

--- adatbazis.sql
+++ adatbazis.sql
@@ -1,3 +1,30 @@
+CREATE DATABASE Lego_Gyar
+USE Lego_Gyar
+
 --Zoli
+CREATE TABLE Alalmazott (
+    TAJ-szám INT PRIMARY KEY
+    Név VARCHAR(30),
+    Email VARCHAR(70),
+    Fizetés INT,
+    SzületésiDátum DATE,
+    Telefonszám INT
+)
+
+CREATE TABLE Beosztás (
+    TAJ int NOT NULL,
+    Gyár_név VARCHAR(50) NOT NULL,
+    CONSTRAINT FK_Beosztás_alkalmazott FOREIGN KEY (TAJ) REFERENCES Alkalmazott (TAJ-szám),
+    CONSTRAINT FK_Beosztás_gyár FOREIGN KEY (Gyár_név) REFERENCES Gyár (Név)
+)
+
+CREATE TABLE Gyártási_Sor (
+    Gyár_név VARCHAR(50) NOT NULL,
+    Szériaszám INT NOT NULL,
+    GyártásiÁr INT,
+    ÁHE INT,
+    CONSTRAINT FK_Beosztás_gyár FOREIGN KEY (Gyár_név) REFERENCES Gyár (Név),
+    CONSTRAINT FK_Beosztás_szett FOREIGN KEY (Szériaszám) REFERENCES Szett (Szériaszám)
+)
 
 --Bogi
