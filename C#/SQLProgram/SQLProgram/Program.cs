350428ed7f1be9c3b8d7d9ff6625ed14439be67f
Fekete Boglárka <fekete.boglarka@ckik.loc>
2024-04-05 11:01:42 +0200

    InsertInto?

--- C#/SQLProgram/SQLProgram/Program.cs
+++ C#/SQLProgram/SQLProgram/Program.cs
@@ -10,26 +10,222 @@ namespace SQLProgram
     class Program
     {
         //VÁLTOZÓK
+        static List<Alkalmazott> alkalmazottak = new List<Alkalmazott>();
+        static List<Alkalmazott> ujalkalmazottak = new List<Alkalmazott>();
+        
+        static List<Gyar> gyarak = new List<Gyar>();
+        static List<Gyar> ujgyarak = new List<Gyar>();
+
+        static List<Beosztas> beosztasok  = new List<Beosztas>();
+        static List<Beosztas> ujbeosztasok  = new List<Beosztas>();
+
+        static List<GyartasiSor> gyarsorok = new List<GyartasiSor>();
+        static List<GyartasiSor> ujgyarsorok = new List<GyartasiSor>();
+
+
         static void Main(string[] args)
         {
             //MAIN
-            Fajlbeolvasas();
+            Fajlbeolvasas1();
+            Fajlbeolvasas2();
+            Fajlbeolvasas3();
+            Fajlbeolvasas4();
+            Redundancia1();
+            Redundancia2();
+            Redundancia3();
+            Redundancia4();
+            InsertFunction1();
+            InsertFunction2();
+            InsertFunction3();
+            InsertFunction4();
 
             //END
             Console.ReadKey();
         }
         //METÓDUSOK
-        static void Fajlbeolvasas()
+        static void InsertFunction1()
+        {
+            Console.WriteLine("INSERT INTO Alkalmazott (");
+            for (int i = 0; i < alkalmazottak.Count; i++)
+            {
+                Alkalmazott a = alkalmazottak[i];
+                Console.WriteLine($" {a.taj}, {a.nev}, {a.email}, {a.fizetes}, {a.datum}, {a.telefonszam}");
+            }
+            Console.WriteLine(");");
+        }
+
+        static void InsertFunction2()
+        {
+            Console.WriteLine("INSERT INTO  (");
+            for (int i = 0; i < .Count; i++)
+            {
+                 a = [i];
+                Console.WriteLine($" {a.}, {}");
+            }
+            Console.WriteLine(");");
+        }
+
+        static void InsertFunction3()
+        {
+            Console.WriteLine("INSERT INTO  (");
+            for (int i = 0; i < .Count; i++)
+            {
+                a = [i];
+                Console.WriteLine($" {a.}, {}");
+            }
+            Console.WriteLine(");");
+        }
+
+        static void InsertFunction4()
+        {
+            Console.WriteLine("INSERT INTO  (");
+            for (int i = 0; i < .Count; i++)
+            {
+                a = [i];
+                Console.WriteLine($" {a.}, {}");
+            }
+            Console.WriteLine(");");
+        }
+
+
+        static void Redundancia1()
+        {
+            for (int i = 0; i < alkalmazottak.Count; i++)
+            {
+                if (!BenneVanE1(alkalmazottak[i]))
+                {
+                    ujalkalmazottak.Add(alkalmazottak[i]);
+                }
+            }
+        }
+
+        static bool BenneVanE1(Alkalmazott a)
+        {
+            int j = 0;
+            while(j < ujalkalmazottak.Count && !a.Equals(ujalkalmazottak[j]))
+            {
+                j++;
+            }
+            return j < ujalkalmazottak.Count;
+        }
+
+        //2
+        static void Redundancia2()
+        {
+            for (int i = 0; i < gyarak.Count; i++)
+            {
+                if (!BenneVanE2(gyarak[i]))
+                {
+                    ujgyarak.Add(gyarak[i]);
+                }
+            }
+        }
+
+        static bool BenneVanE2(Gyar a)
+        {
+            int j = 0;
+            while (j < ujgyarak.Count && !a.Equals(ujgyarak[j]))
+            {
+                j++;
+            }
+            return j < ujgyarak.Count;
+        }
+        
+        //3
+        static void Redundancia3()
+        {
+            for (int i = 0; i < beosztasok.Count; i++)
+            {
+                if (!BenneVanE3(beosztasok[i]))
+                {
+                    ujbeosztasok.Add(beosztasok[i]);
+                }
+            }
+        }
+
+        static bool BenneVanE3(Beosztas a)
+        {
+            int j = 0;
+            while (j < ujbeosztasok.Count && !a.Equals(ujbeosztasok[j]))
+            {
+                j++;
+            }
+            return j < ujbeosztasok.Count;
+        }
+
+        //4
+        static void Redundancia4()
+        {
+            for (int i = 0; i < gyarsorok.Count; i++)
+            {
+                if (!BenneVanE4(gyarsorok[i]))
+                {
+                    ujgyarsorok.Add(gyarsorok[i]);
+                }
+            }
+        }
+
+        static bool BenneVanE4(GyartasiSor a)
+        {
+            int j = 0;
+            while (j < ujgyarsorok.Count && !a.Equals(ujgyarsorok[j]))
+            {
+                j++;
+            }
+            return j < ujgyarsorok.Count;
+        }
+
+        //FájlBeolvasás
+        static void Fajlbeolvasas1()
         {
             string[] adatok = File.ReadAllLines(".txt");
             for (int i = 0; i < adatok.Length; i++)
             {
                 string[] st = adatok[i].Split(' ');
+                Alkalmazott sv = new Alkalmazott(st[0], st[1], st[2], st[3], Convert.ToInt32(st[4]), Convert.ToInt32(st[5]));
+                alkalmazottak.Add(sv);
             }
             // insert into .... (...),
             //(...)
             //(...)
         }
+
+        static void Fajlbeolvasas2()
+        {
+            string[] adatok = File.ReadAllLines(".txt");
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string[] st = adatok[i].Split(' ');
+                Gyar sv = new Gyar(st[0], st[1], Convert.ToInt32(st[2]), Convert.ToInt32(st[3]), Convert.ToInt32(st[4]));
+                gyarak.Add(sv);
+            }
+            
+        }
+
+        static void Fajlbeolvasas3()
+        {
+            string[] adatok = File.ReadAllLines(".txt");
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string[] st = adatok[i].Split(' ');
+                Beosztas sv = new Beosztas(Convert.ToInt32(st[0]), st[1]);
+                beosztasok.Add(sv);
+            }
+
+        }
+
+        static void Fajlbeolvasas4()
+        {
+            string[] adatok = File.ReadAllLines(".txt");
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string[] st = adatok[i].Split(' ');
+                GyartasiSor sv = new GyartasiSor(st[0], Convert.ToInt32(st[1]), Convert.ToInt32(st[1]), Convert.ToInt32(st[2]));
+                gyarsorok.Add(sv);
+            }
+
+        }
+
     }
     //STRUCT
     struct Alkalmazott
@@ -47,6 +243,11 @@ namespace SQLProgram
             taj = ujtaj;
             fizetes = ujfizetes;
         }
+
+        public string nevkiir()
+        {
+            return "Alkalmazott";
+        }
     }
 
     struct Gyar
@@ -62,8 +263,14 @@ namespace SQLProgram
             epitesiev = ujepitesiev;
             bevetel = ujbevetel;
         }
+
+        public string nevkiir()
+        {
+            return "Gyar";
+        }
     }
 
+    /*
     struct Szett
     {
         public string nev, tema;
@@ -82,6 +289,7 @@ namespace SQLProgram
             forgalomban = ujforgalomban;
         }
     }
+    */
 
     struct Beosztas
     {
@@ -92,6 +300,11 @@ namespace SQLProgram
         {
             taj = ujtaj;
             gyarnev = ujgyarnev;
+        }
+
+        public string nevkiir()
+        {
+            return "Beosztas";
         }
     }
 
@@ -106,6 +319,16 @@ namespace SQLProgram
             szeriaszam = ujszeriaszam;
             gyartasiar = ujgyartasiar;
             ahe = ujahe;
+        }
+
+        public string nevkiir()
+        {
+            return "GyartasiSor";
+        }
+
+        public override string ToString()
+        {
+            return $"{gyarnev}, {szeriaszam}, {gyartasiar}, {ahe}";
         }
     }
 }
